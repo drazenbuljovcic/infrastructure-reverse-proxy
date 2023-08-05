377412fcc5f62ae2a3e73133ad39750ed5f24c17
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2023-08-05 20:21:21 +0200

    instrumentation

--- go.mod
+++ go.mod
@@ -3,3 +3,16 @@ module reverse-proxy
 go 1.20
 
 require github.com/joho/godotenv v1.5.1 // direct
+
+require (
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/openzipkin/zipkin-go v0.4.1 // indirect
+	go.opentelemetry.io/otel v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/zipkin v1.16.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -8,7 +11,41 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
+
+// newExporter returns a console exporter.
+func newExporter(w io.Writer) (trace.SpanExporter, error) {
+	return stdouttrace.New(
+		stdouttrace.WithWriter(w),
+		// Use human-readable output.
+		stdouttrace.WithPrettyPrint(),
+		// Do not print timestamps for the demo.
+		stdouttrace.WithoutTimestamps(),
+	)
+}
+
+// newResource returns a resource describing this application.
+func newResource() *resource.Resource {
+	r, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(os.Getenv("OTEL_SERVICE_NAME")),
+			semconv.ServiceVersion("v0.1.0"),
+			attribute.String("environment", "demo"),
+		),
+	)
+	return r
+}
 
 func logRequestsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +63,28 @@ func reverseProxyHandlerMiddleware(targetURL *url.URL) http.HandlerFunc {
 		r.URL.Host = targetURL.Host
 		r.URL.Scheme = targetURL.Scheme
 
+		// Extract traceparent from the incoming request
+		ctx := r.Context()
+		traceparent := r.Header.Get("traceparent")
+		log.Printf("traceparent: %s", traceparent)
+		if traceparent != "" {
+			ctx = propagation.TraceContext{}.Extract(ctx, propagation.HeaderCarrier(r.Header))
+		}
+
+		_, span := otel.Tracer(os.Getenv("OTEL_SERVICE_NAME")).Start(ctx, r.URL.Path)
+		defer span.End()
+		
+		childTraceparent := fmt.Sprintf("00-%s-%s-01", span.SpanContext().TraceID().String(), span.SpanContext().SpanID().String())
+		// for key, values := range r.Header {
+		// 	for _, value := range values {
+		// 		r.Header.Add(key, value)
+		// 	}
+		// }
+
+		// log.Printf(span.SpanContext().TraceFlags().String())
+		r.Header.Del("traceparent")
+		r.Header.Add("traceparent", string(childTraceparent))
+
 		// Perform the reverse proxy
 		proxy.ServeHTTP(w, r)
 	}
@@ -41,7 +100,36 @@ func main() {
 	// backendURL, err := url.Parse("http://172.25.0.3:3000")
 	reverseProxyBackendHost := os.Getenv("SERVICE_HOSTNAME")
 
-	log.Print("HOSTNAME: ", reverseProxyBackendHost)
+	// write traces to a file? For some reason
+	l := log.New(os.Stdout, "", 0)
+	f, err := os.Create("traces.txt")
+	if err != nil {
+		l.Fatal(err)
+	}
+	defer f.Close()
+
+	// set up the exporter
+	// exp, err := newExporter(f)
+	// if err != nil {
+	// 	l.Fatal(err)
+	// }
+
+	exporter, err := zipkin.New(
+		os.Getenv("OTEL_API_HOST") + "/api/v2/spans",
+	)
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(newResource()),
+	)
+
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			l.Fatal(err)
+		}
+	}()
+	otel.SetTracerProvider(tp)
+	
 	backendURL, err := url.Parse(reverseProxyBackendHost)
 	if err != nil {
 		log.Fatal("Error parsing backend URL:", err)
